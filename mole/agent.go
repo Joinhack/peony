b0a0a93aded02fb07f08b48abc4efd80cc70750b
joinhack <joinhack@gmail.com>
2013-11-09 00:08:00 +0800

    add notify.go

--- mole/agent.go
+++ mole/agent.go
@@ -6,23 +6,59 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
 type Agent struct {
-	app     *peony.App
-	AppAddr string
-	proxy   *httputil.ReverseProxy
+	app      *peony.App
+	AppAddr  string
+	notifier *peony.Notifier
+	proxy    *httputil.ReverseProxy
+}
+
+func (a *Agent) Path() string {
+	return a.app.AppPath
+}
+
+func (a *Agent) Apply() {
+	println(111)
+}
+
+func (a *Agent) IgnoreDir(info os.FileInfo) bool {
+	switch info.Name() {
+	case "tmp", "view":
+		return true
+	default:
+		return false
+	}
+}
+
+func (a *Agent) IgnoreFile(info os.FileInfo) bool {
+	if strings.HasSuffix(info.Name(), ".go") {
+		return false
+	}
+	return true
 }
 
 func NewAgent(app *peony.App, appAddr string) (agent *Agent, err error) {
 	agent = &Agent{app: app}
 	targetSvrUrl := &url.URL{Scheme: "http", Host: appAddr}
 	agent.proxy = httputil.NewSingleHostReverseProxy(targetSvrUrl)
+	agent.notifier = peony.NewNotifier()
+	agent.notifier.Watch(agent)
 	return
 }
 
+func processError(err error, w http.ResponseWriter) {
+	w.Write([]byte(err.Error()))
+}
+
 func (a *Agent) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := a.notifier.Notify()
+	if err != nil {
+		processError(err, w)
+		return
+	}
 	a.proxy.ServeHTTP(w, r)
 }
 
--- notify.go
+++ notify.go
@@ -4,46 +4,97 @@ import (
 	"github.com/howeyc/fsnotify"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Observer interface {
-	Action()
+	Apply()
 	Path() string
 }
 
 type IgnoreObserver interface {
 	Observer
 	IgnoreDir(file os.FileInfo) bool
-	IgnoreFile(file os.FileInfo) bool
+	IgnoreFile(file string) bool
+}
+
+type observerWatcher struct {
+	observer Observer
+	watcher  *fsnotify.Watcher
+	path     string
 }
 
 type Notifier struct {
-	watcher *fsnotify.Watcher
+	watchers []*observerWatcher
 }
 
 func NewNotifier() *Notifier {
 	n := &Notifier{}
+	n.watchers = []*observerWatcher{}
 	return n
 }
 
+func (n *Notifier) contain(abspath string) bool {
+	for _, obswatcher := range n.watchers {
+		if obswatcher.path == abspath {
+			return true
+		}
+	}
+	return false
+}
+
 func (n *Notifier) Watch(o Observer) {
+	var err error
+	var abspath string
+	abspath, err = filepath.Abs(o.Path())
+	if err != nil {
+		ERROR.Println("create watcher error:", err)
+		return
+	}
+	if n.contain(abspath) {
+		return
+	}
+	var watcher *fsnotify.Watcher
+	watcher, err = fsnotify.NewWatcher()
+	obsWatcher := &observerWatcher{o, watcher, abspath}
+	n.watchers = append(n.watchers, obsWatcher)
+
 	var ignoreObserver IgnoreObserver = nil
 	var isIgnoreObserver bool
 	ignoreObserver, isIgnoreObserver = o.(IgnoreObserver)
-	n.watcher.Watch(o.Path())
-	filepath.Walk(o.Path(), func(path string, info os.FileInfo, err error) error {
+	watcher.Watch(abspath)
+	filepath.Walk(abspath, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			return nil
+		}
 		if isIgnoreObserver {
 			if info.IsDir() && ignoreObserver.IgnoreDir(info) {
 				return filepath.SkipDir
 			}
-			if ignoreObserver.IgnoreFile(info) {
-				return
-			}
 		}
-		n.watcher.Watch(path)
+		watcher.Watch(path)
+		return nil
 	})
 }
 
-func (n *Notifier) Notify() {
-
+func (n *Notifier) Notify() error {
+	for _, obswatcher := range n.watchers {
+		select {
+		case evt := <-obswatcher.watcher.Event:
+			//ignore file name start with "." like ".xxx"
+			if !strings.HasPrefix(filepath.Base(evt.Name), ".") {
+				if ignoreObserver, ok := obswatcher.observer.(IgnoreObserver); ok {
+					if ignoreObserver.IgnoreFile(evt.Name) {
+						continue
+					}
+				}
+			}
+		case err := <-obswatcher.watcher.Error:
+			return err
+		default:
+			continue
+		}
+		obswatcher.observer.Apply()
+	}
+	return nil
 }
