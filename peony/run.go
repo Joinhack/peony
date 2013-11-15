ee541627a16591e8b87b280110f5a78e70a8af18
joinhack <joinhack@gmail.com>
2013-11-16 00:08:13 +0800

    add command suport

---
+++ peony/run.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/joinhack/peony"
+	"github.com/joinhack/peony/mole"
+)
+
+var runcmd = &Command{Name: "run", Execute: run}
+
+func run(args []string) {
+	if len(args) == 0 {
+		usage(1)
+	}
+	importPath := args[0]
+	srcRoot := peony.SearchSrcRoot(importPath)
+	app := peony.NewApp(srcRoot, importPath)
+	ag, _ := mole.NewAgent(app)
+	ag.Run(":8080")
+}
