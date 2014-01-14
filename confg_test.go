eb65477abdcf585c78947b299e4f06c798748a75
joinhack <joinhack@gmail.com>
2014-01-14 22:18:31 +0800

    config

---
+++ confg_test.go
@@ -0,0 +1,66 @@
+package peony
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestConfig1(t *testing.T) {
+	content := `
+key1=1
+#comment
+key2 = 2.2
+key3 = y
+
+=
+
+	`
+	lines := strings.Split(content, "\n")
+	conf := Config{}
+	if err := conf.readLines(lines); err != nil {
+		t.Error(err)
+	}
+	t.Log(conf.Float("", "key2"))
+	t.Log(conf.Int("", "key1"))
+	t.Log(conf.Bool("", "key3"))
+	t.Log(conf.String("", ""))
+}
+
+func TestConfig2(t *testing.T) {
+	content := `
+key1=1
+#comment
+key2 = 2.2
+key3 = y
+
+key4
+
+	`
+	lines := strings.Split(content, "\n")
+	conf := Config{}
+	if err := conf.readLines(lines); err != nil {
+		t.Log(err)
+	}
+
+}
+
+func TestConfig3(t *testing.T) {
+	content := `
+	[s1]
+	key1=1
+	#comment
+	key2 = 2.2
+	key3 = 10.2
+	key3 = n
+	`
+	lines := strings.Split(content, "\n")
+	conf := Config{}
+	if err := conf.readLines(lines); err != nil {
+		t.Log(err)
+	}
+	t.Log(conf)
+	t.Log(conf.Float("s1", "key2"))
+	t.Log(conf.Int("s1", "key1"))
+	t.Log(conf.Float("s1", "key3"))
+	t.Log(conf.Bool("s1", "key3"))
+}
--- config.go
+++ config.go
@@ -39,21 +39,19 @@ func (c Config) HasSection(name string) bool {
 	return false
 }
 
-func ConfigRead(path string) (conf Config, err error) {
-	var lines []string
-	lines, err = ReadLines(path)
-	if err != nil {
-		return
-	}
+func (conf Config) readLines(lines []string) (err error) {
 	var secname string
 	var section Section
-	conf = make(Config)
 	for l, line := range lines {
 		var idx int
 		line = strings.Trim(line, " \t\n")
+		if len(line) == 0 || line[0] == '#' {
+			//skip empty line or coment line
+			continue
+		}
 		lineLen := len(line)
 		if len(line) >= 2 && line[0] == '[' && line[lineLen-1] == ']' {
-			secname = line[1 : lineLen-2]
+			secname = line[1 : lineLen-1]
 			section = nil
 			continue
 		}
@@ -67,7 +65,7 @@ func ConfigRead(path string) (conf Config, err error) {
 		}
 
 		if idx = strings.Index(line, "="); idx == -1 {
-			err = errors.New(fmt.Sprintf("unkown key value expr, line:", l))
+			err = errors.New(fmt.Sprintf("key value expr error, @line: %d", l))
 			return
 		}
 		key := strings.Trim(line[0:idx], " \t\n")
@@ -78,6 +76,16 @@ func ConfigRead(path string) (conf Config, err error) {
 		}
 		section[key] = item
 	}
+	return
+}
+
+func (conf Config) ReadFile(path string) (err error) {
+	var lines []string
+	lines, err = ReadLines(path)
+	if err != nil {
+		return
+	}
+	err = conf.readLines(lines)
 	return
 }
 
@@ -97,33 +105,65 @@ func (c Config) getItem(secname, key string) *citem {
 
 func (c Config) Bool(secname, key string) (rs, ok bool) {
 	var item *citem
-	if item = c.getItem(secname, key); item == nil {
-		ok = false
+	ok = false
+	for item = c.getItem(secname, key); item != nil; item = item.Next {
+		val := strings.ToLower(item.Value)
+		rs, ok = boolMapping[val]
+		if !ok {
+			continue
+		}
+		ok = true
 		return
 	}
-	val := strings.ToLower(item.Value)
-	rs, ok = boolMapping[val]
 	return
 }
 
 func (c Config) Int(secname, key string) (rs int64, ok bool) {
 	var item *citem
-	if item = c.getItem(secname, key); item == nil {
-		ok = false
+	ok = false
+	for item = c.getItem(secname, key); item != nil; item = item.Next {
+		var err error
+		if strings.HasPrefix(item.Value, "0x") || strings.HasPrefix(item.Value, "0X") {
+			if rs, err = strconv.ParseInt(item.Value, 16, 64); err != nil {
+				continue
+			}
+			ok = true
+			return
+		}
+
+		if strings.HasPrefix(item.Value, "o") || strings.HasPrefix(item.Value, "O") {
+			if rs, err = strconv.ParseInt(item.Value, 8, 64); err != nil {
+				continue
+			}
+			ok = true
+			return
+		}
+		if rs, err = strconv.ParseInt(item.Value, 10, 64); err != nil {
+			continue
+		}
+		ok = true
 		return
 	}
+	return
+}
+
+func (c Config) Float(secname, key string) (rs float64, ok bool) {
+	var item *citem
+	ok = false
 	var err error
-	if rs, err = strconv.ParseInt(item.Value, 10, 64); err != nil {
-		ok = false
+	for item = c.getItem(secname, key); item != nil; item = item.Next {
+		if rs, err = strconv.ParseFloat(item.Value, 64); err != nil {
+			continue
+		}
+		ok = true
+		return
 	}
-	ok = true
 	return
 }
 
 func (c Config) String(secname, key string) (rs string, ok bool) {
 	var item *citem
 	if item = c.getItem(secname, key); item == nil {
-		ok = false
 		return
 	}
 	ok = true
