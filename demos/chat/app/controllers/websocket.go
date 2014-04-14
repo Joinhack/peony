d133a8d20843048b7c516b5ede11f261327c91bd
joinhack <joinhack@gmail.com>
2014-04-14 16:47:51 +0800

    format change

--- demos/chat/app/controllers/websocket.go
+++ demos/chat/app/controllers/websocket.go
@@ -208,7 +208,7 @@ func (c *WebSocket) chat(ws *websocket.Conn) {
 		}
 		msg.From = client.clientId
 		now := time.Now()
-		msg.Time = now.UnixNano() / 1000
+		msg.Time = now.UnixNano() / 1000000
 		reply := NewReplySuccessMsg(client.clientId, msg.MsgId, msg.Time)
 		client.SendMsg(reply)
 
