465d93772d30c1ce0ddf1074839b9b3c3ab09728
joinhack <joinhack@gmail.com>
2014-06-27 18:09:58 +0800

    change test

--- demos/chat/client/msgcli.go
+++ demos/chat/client/msgcli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
+	c "github.com/joinhack/peony/demos/chat/app/controllers"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -14,18 +15,10 @@ var (
 	host    *string = flag.String("host", "127.0.0.1", "destination hosts")
 	port    *int    = flag.Int("port", 80, "the port")
 	delay   *int    = flag.Int("delay", 5, "delay")
-	start   *int    = flag.Int("start", 0, "start")
+	start   *int    = flag.Int("start", 1, "start")
 	uri     *string = flag.String("uri", "/", "the port")
-	connNum *int    = flag.Int("nums", 100, "the connection numbers")
+	connNum *int    = flag.Int("nums", 2, "the connection numbers")
 )
-
-type Msg struct {
-	Code    int
-	Type    int
-	From    int64
-	To      int64
-	Content string
-}
 
 type SumInfo struct {
 	SendInfo map[int64]int64
@@ -59,7 +52,7 @@ func main() {
 	var startNum = *start
 	var endNum = *start + *connNum
 	for i := startNum; i < endNum; i++ {
-		var login = &Msg{Code: 0, From: int64(i), Type: 0}
+		var login = &c.RegisterMsg{DevType: 1, Id: uint64(i), Type: c.LoginMsgType}
 		ws, err := websocket.Dial(target+fmt.Sprintf("?user=[%d]", i), "", origin)
 		if err != nil {
 			panic(err)
@@ -70,39 +63,43 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+		rs := &c.Msg{}
+		if err = websocket.JSON.Receive(ws, rs); err != nil {
+			panic(err)
+		}
+		replyCount++;
+
 		go func() {
 			for {
-				rs := &Msg{}
+				rs := &c.Msg{}
 				err = websocket.JSON.Receive(ws, rs)
 				if err != nil {
 					fmt.Println(err)
 					return
 				}
 				if rs.Type == 1 {
-					replyCount++
-				}
-				if rs.Type == 2 {
 					TotalRecv++
 				}
 			}
 		}()
 
 		go func(i int) {
-			var msg Msg
-			msg.Content = fmt.Sprintf("%d said: 12121212asdasd", i)
+			var msg c.Msg
+			content := fmt.Sprintf("%d said: 12121212asdasd", i)
+			msg.Content = &content
 			for replyCount < *connNum {
 				time.Sleep(100000 * time.Microsecond)
 			}
 			for running {
 				for {
-					to := rand.Intn(*connNum) + startNum
-					if to != i {
-						msg.To = int64(to)
+					to := uint64(rand.Intn(*connNum) + startNum)
+					if to != uint64(i) {
+						msg.To = &to
 						break
 					}
 				}
-				msg.Type = 2
-				msg.From = int64(i)
+				msg.Type = 1
+				msg.MsgId = fmt.Sprintf("%d", msg.To);
 				err := websocket.JSON.Send(ws, &msg)
 				if err != nil {
 					fmt.Println(err)
@@ -117,7 +114,7 @@ func main() {
 	signal.Notify(signalChan)
 	<-signalChan
 	running = false
-	time.Sleep(30 * time.Second)
+	time.Sleep(10 * time.Second)
 	var con *websocket.Conn
 	for _, con = range peers {
 		con.Close()
