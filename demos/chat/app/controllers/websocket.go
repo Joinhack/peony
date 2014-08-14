73d7e9a5fedef69d61c8f29be4698f1a1e560d5c
joinhack <joinhack@gmail.com>
2014-08-14 15:35:06 +0800

    add parameter check.

--- demos/chat/app/controllers/websocket.go
+++ demos/chat/app/controllers/websocket.go
@@ -534,10 +534,6 @@ func (c *WebSocket) chat(ws *websocket.Conn) {
 			}
 			return
 		}
-		if msg.SourceType != 1 && msg.SourceType != 3 {
-			ws.Write(InvaildParameters)
-			return
-		}
 		if msg.SourceType == 1 && msg.To == nil {
 			ws.Write(InvaildParameters)
 			return
